037b6106367b9dff55d2e8722251147e4426f0fa
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-20 22:14:53 +0300

    Add components for sidebar and scene

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
 
+import { Scene } from './Scene.js'
+import { Sidebar } from './Sidebar.js'
+
 class App extends Component {
   render () {
     return (
       <Grid columns={2} divided>
         <GridRow>
           <GridColumn width={2}>
-            AAAAA
+            <Sidebar/>
           </GridColumn>
           <GridColumn width={8}>
-            BBBBB
+            <Scene/>
           </GridColumn>
         </GridRow>
       </Grid>
