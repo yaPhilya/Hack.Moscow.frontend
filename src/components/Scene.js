9c3a197e57e1fef2cd57c058f41a7a2a950d1571
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-21 23:09:31 +0300

    Remove stl viewer.

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,122 +1,109 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {
+  Scene as ThreeScene,
+  WebGLRenderer,
+  Loader,
+  Mesh,
+  PerspectiveCamera,
+  MeshBasicMaterial,
+} from 'three'
 import PropTypes from 'prop-types'
-import STLViewer from 'stl-viewer'
 
-import apple from '../models/Apple.stl'
-import armchair from '../models/Armchair.stl'
-import bear from '../models/Bear.stl'
-import boat from '../models/Boat.stl'
-import bookCase from '../models/Bookcase.stl'
-import bottle from '../models/Bottle.stl'
-import building from '../models/Building.stl'
-import bus from '../models/Bus.stl'
-import butterfly from '../models/Butterfly.stl'
-import campfire from '../models/Campfire.stl'
-import cat from '../models/Cat.stl'
-import chimp from '../models/Chimp.stl'
-import clownFish from '../models/Clownfish.stl'
-import container from '../models/Container.stl'
-import crab from '../models/Crab.stl'
-import deer from '../models/Deer.stl'
-import dog from '../models/Dog.stl'
-import dragon from '../models/Dragon.stl'
-import fish from '../models/Fish.stl'
-import fish1 from '../models/Fish1.stl'
-import fish2 from '../models/Fish2.stl'
-import fish3 from '../models/Fish3.stl'
-import fish4 from '../models/Fish4.stl'
-import fish5 from '../models/Fish5.stl'
-import fish6 from '../models/Fish6.stl'
-import fork from '../models/Fork.stl'
-import gazebo from '../models/Gazebo.stl'
-import giraffe from '../models/Giraffe.stl'
-import house from '../models/House.stl'
-import house2 from '../models/House2.stl'
-import mug from '../models/Mug.stl'
-import orange from '../models/Orange.stl'
-import pig from '../models/Pig.stl'
-import pillow from '../models/Pillow.stl'
-import rhino from '../models/Rhino.stl'
-import spiders from '../models/Spiders.stl'
-import spoon from '../models/Spoon.stl'
-import starDestroyer from '../models/StarDestroyer.stl'
-import streamLocomotive from '../models/SteamLocomotive.stl'
-import table from '../models/Table.stl'
-import tank from '../models/Tank.stl'
-import teeCup from '../models/Tea Cup.stl'
-import tree from '../models/Tree.stl'
-import trees from '../models/Trees.stl'
-import triceratops from '../models/Triceratops.stl'
-import wolf from '../models/Wolf.stl'
-
-const modelsStorage = {
-  apple,
-  armchair,
-  bear,
-  boat,
-  bookCase,
-  bottle,
-  building,
-  bus,
-  butterfly,
-  campfire,
-  cat,
-  chimp,
-  clownFish,
-  container,
-  crab,
-  deer,
-  dog,
-  dragon,
-  fish,
-  fish1,
-  fish2,
-  fish3,
-  fish4,
-  fish5,
-  fish6,
-  fork,
-  gazebo,
-  giraffe,
-  house,
-  house2,
-  mug,
-  orange,
-  pig,
-  pillow,
-  rhino,
-  spiders,
-  spoon,
-  starDestroyer,
-  streamLocomotive,
-  table,
-  tank,
-  teeCup,
-  tree,
-  trees,
-  triceratops,
-  wolf,
-}
+import modelsStorage from '../models/models'
 
 // Костыль, чтобы работало без пробрасывания размеров
 const magicSizes = {
-  width: 1200,
-  height: 800,
+  width: '1200px',
+  height: '800px',
 }
 
-const Scene = ({models}) => <STLViewer
-  url={modelsStorage[models[0].name]}
-  width={magicSizes.width}
-  height={magicSizes.height}
-  modelColor='#B92C2C'
-  backgroundColor='#EAEAEA'
-  rotate={true}
-  orbitControls={true}
-/>
+class Scene extends Component {
+  constructor (props) {
+    super(props)
+
+    this.start = this.start.bind(this)
+    this.stop = this.stop.bind(this)
+    this.animate = this.animate.bind(this)
+  }
+
+  componentDidMount () {
+    const models = this.props.models
+
+    const width = this.mount.width
+    const height = this.mount.height
+
+    const scene = new ThreeScene()
+    const camera = new PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      1000,
+    )
+    const renderer = new WebGLRenderer({antialias: true})
+    const loader = new Loader()
+
+    models.forEach(modelData => {
+      loader.load(modelsStorage[modelData.name], (geometry) => {
+        const material = new MeshBasicMaterial({color: '#433F81'})
+        const mesh = new Mesh(geometry, material)
+
+        scene.add(mesh)
+      })
+    })
+
+    camera.position.z = 4
+    renderer.setClearColor('#000000')
+    renderer.setSize(width, height)
+
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+
+    this.mount.appendChild(this.renderer.domElement)
+    this.start()
+  }
+
+  componentWillUnmount () {
+    this.stop()
+    this.mount.removeChild(this.renderer.domElement)
+  }
+
+  start () {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate)
+    }
+  }
+
+  stop () {
+    cancelAnimationFrame(this.frameId)
+  }
+
+  animate () {
+    this.renderScene()
+    this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  renderScene () {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  render () {
+    return (
+      <div
+        style={{width: magicSizes.width, height: magicSizes.height}}
+        ref={(mount) => { this.mount = mount }}
+      />
+    )
+  }
+}
 
 Scene.propTypes = {
   models: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string,
+      color: PropTypes.string,
+      x: PropTypes.number,
+      y: PropTypes.number,
+      z: PropTypes.number,
     }),
   ),
 }
