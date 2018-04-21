2148748d78ab6961d19ad26b1099992c36b23415
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-21 20:16:47 +0300

    Simple fetcher working

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
 
-const Sidebar = () => (
-  <Form>
+const Sidebar = ({fetchExtract}) => (
+  <Form onSubmit={() => fetchExtract('aaaaa')}>
     <Form.Field label="Хочу картинок" control="textarea" rows="3"/>
     <Form.Field control={Button}>ТЫЦ</Form.Field>
   </Form>
 )
+
+Sidebar.propTypes = {
+  fetchExtract: PropTypes.func,
+}
 
 export default Sidebar
--- src/sagas.js
+++ src/sagas.js
@@ -1,12 +1,12 @@
 import { all, takeEvery, put } from 'redux-saga/effects'
 import { FETCH_EXTRACT_REQUEST, setModelsCreator } from './actions'
 
-function * fetchAsync () {
-  console.log('Fetch')
+function * fetchAsync (text) {
+  console.log('Fetch: ' + text)
 }
 
 function * extractSaga () {
-  yield takeEvery(FETCH_EXTRACT_REQUEST, fetchAsync)
+  yield takeEvery(FETCH_EXTRACT_REQUEST, fetchAsync, 'aaaa')
 
   yield put(setModelsCreator([
     {
