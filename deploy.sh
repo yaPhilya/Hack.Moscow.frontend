6eb448c619979e47c2adc2ebd9194b2abcd69128
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-22 11:08:12 +0300

    Fix deploy.

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -e
 docker build -t strangeducttape/bluecat_frontend .
 docker push strangeducttape/bluecat_frontend
 
-scp ./docker-compose.yaml root@188.246.233.30:/app/frontend
+scp ./docker-compose.yaml root@188.246.233.30:/app
 
-ssh root@188.246.233.30 "cd /app/frontend && docker stack deploy --compose-file docker-compose.yaml frontend"
+ssh root@188.246.233.30 "cd /app && docker stack deploy --compose-file docker-compose.yaml images"
