45f6848e649e740ca9201bb3655e6ada59261aa1
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-22 11:16:02 +0300

    FIX

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://backend:5000/parse'
+const API_URL = 'http://188.246.233.30:5000/parse'
 
 export const doParseRequest = text => {
   return axios.post(API_URL, {
