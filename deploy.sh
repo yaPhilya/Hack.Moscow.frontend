ac6ca4468840c524542c65adacfe98d61ce84073
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-22 04:10:05 +0300

    Fix dir name

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 docker build -t strangeducttape/bluecat_frontend .
-docker push strangeducttape/bluecat_frontend
+docker push strangeducttape /bluecat_frontend
 
 scp ./docker-compose.yaml root@188.246.233.30:/app/frontend
 
-ssh root@188.246.233.30 "cd /app/backend && docker stack deploy --compose-file docker-compose.yaml frontend"
+ssh root@188.246.233.30 "cd /app/frontend && docker stack deploy --compose-file docker-compose.yaml frontend"
